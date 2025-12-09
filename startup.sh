90c874a1f446a99695033a69bcb315651911b470
Marcel <marcel.kowalik.rekrutacja@gmail.com>
2025-12-09 19:06:12 +0100

    logdir

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ set +a
 
 python -m pip install -r requirements.txt
 
-LOG_FILE="${PERSISTENT_STORAGE}/logs/worker_${POD_ID}.txt"
+LOG_FILE="${PERSISTENT_STORAGE}/fineweb2_bis/data_labeling/logs/worker_${POD_ID}.txt"
 mkdir -p "${PERSISTENT_STORAGE}/logs"
 exec > >(tee -a "$LOG_FILE") 2>&1
 
